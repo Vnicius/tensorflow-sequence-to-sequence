ed649977f91fda2a670bd7823ffb43faf4fe9d2c
lavid <vinicius.matheus252@gmail.com>
2018-03-19 10:22:14 -0300

    In-out  batch

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,50 @@
 import numpy as np
 
+def batch_in_out(inputs, outputs, max_sequence_length=None):
+    """
+    Args:
+        inputs:
+            list of sentences (integer lists)
+        max_sequence_length:
+            integer specifying how large should `max_time` dimension be.
+            If None, maximum sequence length would be used
+    
+    Outputs:
+        inputs_time_major:
+            input sentences transformed into time-major matrix 
+            (shape [max_time, batch_size]) padded with 0s
+        sequence_lengths:
+            batch-sized list of integers specifying amount of active 
+            time steps in each input sequence
+    """
+    
+    sequence_input_lengths = [len(seq) for seq in inputs]
+    sequence_output_lengths = [len(seq) for seq in outputs]
+    batch_size = len(inputs)
+    
+    if max_sequence_length is None:
+        max_sequence_length_in = max(sequence_input_lengths)
+        max_sequence_length_out = max(sequence_output_lengths)
+        max_sequence_length = max_sequence_length_in if max_sequence_length_in > max_sequence_length_out else max_sequence_length_out
+    
+    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], dtype=np.int32) # == PAD
+    outputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], dtype=np.int32)
+    
+    for i, seq in enumerate(inputs):
+        for j, element in enumerate(seq):
+            inputs_batch_major[i, j] = element
+
+    for i, seq in enumerate(outputs):
+        for j, element in enumerate(seq):
+            outputs_batch_major[i, j] = element
+
+
+    # [batch_size, max_time] -> [max_time, batch_size]
+    inputs_time_major = inputs_batch_major.swapaxes(0, 1)
+    outputs_time_major = outputs_batch_major.swapaxes(0, 1)
+
+    return inputs_time_major, outputs_time_major
+
 def batch(inputs, max_sequence_length=None):
     """
     Args:
